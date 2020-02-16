5ab052b3836cd8c23cd076efc8ae4faca34f0952
glass <glass.ships@outlook.com>
2020-02-16 14:33:39 -0700

    no compiley but gettign there

---
+++ docs/handwritten.md
@@ -0,0 +1,2 @@
+To compile a test, use the following command:
+  `g++ -I/opt/awkward-1.0/include -L/opt/awkward-1.0/build/lib.linux-x86_64-3.7/awkward1 toy_data_awkward.cpp -lawkward-static -lawkward-cpu-kernels-static -o Test`
--- kaitai/awkward_example/src/animal.cpp
+++ kaitai/awkward_example/src/animal.cpp
@@ -3,32 +3,84 @@
 #include <memory>
 #include "animal.h"
 
-
+namespace ak = awkward;
 
 animal_t::animal_t(kaitai::kstream* p__io, kaitai::kstruct* p__parent, animal_t* p__root) : kaitai::kstruct(p__io) {
     m__parent = p__parent;
     m__root = this;
-    m_entry = 0;
+    //m_entry = 0;
+    //ak::FillableArray m_entry(ak::FillableOptions(1024, 2.0));
     _read();
 }
 
-
+//object version
 void animal_t::_read() {
-    m_entry = new std::vector<animal_entry_t*>();
+    //ak::FillableArray m_entry(ak::FillableOptions(1024, 2.0));
     {
         int i = 0;
         while (!m__io->is_eof()) {
-            m_entry->push_back(new animal_entry_t(m__io, this, m__root));
+            animal_entry_t this_animal = animal_entry_t(m__io, this, m__root);
+           
+            // start record for i-th animal
+            m_entry.beginrecord();
+
+            // record species name
+            m_entry.field_check("Name");
+            m_entry.string(this_animal.species());
+
+            // record age
+            m_entry.field_check("Age");
+            m_entry.integer(this_animal.age());
+        
+            // record weight
+            m_entry.field_check("Weight");
+            m_entry.integer(this_animal.weight());
+        
+            // end record for i-th animal
+            m_entry.endrecord();
+
             i++;
         }
     }
 }
 
+// pointer version
+// void animal_t::_read() {
+//     ak::FillableArray m_entry(ak::FillableOptions(1024, 2.0));
+//     {
+//         int i = 0;
+//         while (!m__io->is_eof()) {
+//             animal_entry_t this_animal = animal_entry_t(m__io, this, m__root);
+           
+//             // start record for i-th animal
+//             m_entry.beginrecord();
+
+//             // record species name
+//             m_entry.field_check("Name");
+//             m_entry.string(this_animal->species());
+
+//             // record age
+//             m_entry.field_check("Age");
+//             m_entry.integer(this_animal->age());
+        
+//             // record weight
+//             m_entry.field_check("Weight");
+//             m_entry.integer(this_animal->weight());
+        
+//             // end record for i-th animal
+//             m_entry.endrecord();
+
+//             i++;
+//         }
+//     }
+// }
+
 animal_t::~animal_t() {
-    for (std::vector<animal_entry_t*>::iterator it = m_entry->begin(); it != m_entry->end(); ++it) {
-        delete *it;
-    }
-    delete m_entry;
+//    for (std::vector<animal_entry_t*>::iterator it = m_entry.begin(); it != m_entry.end(); ++it) {
+//        delete *it;
+//    }
+//   delete m_entry;
+     std::cout << "animal_t destructor (dummy)" << std::endl;
 }
 
 animal_t::animal_entry_t::animal_entry_t(kaitai::kstream* p__io, animal_t* p__parent, animal_t* p__root) : kaitai::kstruct(p__io) {
