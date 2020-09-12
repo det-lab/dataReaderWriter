19b274b25c84149d64354876d75b16f8b6ad3c5c
glass <glass.ships@outlook.com>
2020-09-12 12:41:43 -0600

    finally works! (just not the ksc part)

--- kaitai/awkward_example/src/main.cpp
+++ kaitai/awkward_example/src/main.cpp
@@ -1,29 +1,31 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 #include "animal_reference.h" /* need to include the animal header file we generated */
 
 using namespace std;
 namespace ak = awkward;
 
-int main() {
+int main(int num_args, char** argv) {
 
   cout << "initiate sequence"<< endl; 
 
   /* Open the input file in binary mode */
-  //ifstream infile("../../../data/animal_raw", ifstream::binary);
-  ifstream infile("/home/josh/dev/awkward-kaitai/dataReaderWriter/data/animal_raw", ifstream::binary);
+//ifstream infile("/root/dataReaderWriter/data/animal_raw", ifstream::binary);
+  string input_file(argv[1]);
+  ifstream infile(input_file, ifstream::binary);
 
-  cout << "In-file check" << endl;
+  cout << "data file found" << endl;
 
   /* Create a kaitai stream of the source file */
   kaitai::kstream ks(&infile);
 
-  cout << "kaitai stream check" << endl;
+  cout << "data file converted to kaitai stream" << endl;
 
   /* Get a pointer to the array containing all the animals */
   ak::ArrayBuilder animal_array = read_animal(&ks);
 
-  cout << "awkward array check" << endl;
+  cout << "awkward array built kaitai stream" << endl;
 
   // take a snapshot
   shared_ptr<ak::Content> array = animal_array.snapshot();
